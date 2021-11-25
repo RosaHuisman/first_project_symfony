f40e798ed6c193329dbae1a341f9300c95a9ded0
Rosa Huisman <rosa.huisman@gmail.com>
2021-11-25 16:28:24 +0100

    default2

--- migrations/Version20211123161503.php
+++ migrations/Version20211123161503.php
@@ -20,7 +20,7 @@ final class Version20211123161503 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE justuser (id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY, email VARCHAR(180) NOT NULL UNIQUE, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, telephone VARCHAR(255) NOT NULL, a_propos LONGTEXT DEFAULT NULL, instagram VARCHAR(255) DEFAULT NULL));
+        $this->addSql('CREATE TABLE justuser (id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY, email VARCHAR(180) NOT NULL UNIQUE, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, nom VARCHAR(255) NOT NULL, telephone VARCHAR(255) NOT NULL, a_propos LONGTEXT DEFAULT NULL, instagram VARCHAR(255) DEFAULT NULL) COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
